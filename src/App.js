88832be12678ffab156ec87b6cf76f991a3790bf
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-23 21:05:38 +0300

    react starting folder

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { Button } from 'react-bootstrap';
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecomm Application</h1>
+      <Button>Login</Button>
+    </div>
+  );
+}
+
+export default App;
